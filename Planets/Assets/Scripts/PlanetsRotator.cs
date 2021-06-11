4e19988d746b8b72b354c2990e649df2e066a11a
id-zonex <idzone027@gmail.com>
2021-06-12 02:13:01 +0300

    controller type one update smoothness

--- Planets/Assets/Scripts/PlanetsRotator.cs
+++ Planets/Assets/Scripts/PlanetsRotator.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlanetsRotator : MonoBehaviour
 {
+    [SerializeField] private float sensitivity = 4;
 
     private void LateUpdate()
     {
@@ -20,6 +21,7 @@ public class PlanetsRotator : MonoBehaviour
     {
         var direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position); 
         var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.down);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.down), Time.deltaTime * sensitivity);
     }
 }

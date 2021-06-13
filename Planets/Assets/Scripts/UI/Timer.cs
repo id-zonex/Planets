9348f8fbe64686ed01c5c316eaa2919a9aa54b2d
vay4ie <vstukalovs@gmail.com>
2021-06-13 13:08:40 +0300

    Grand Commit
    Добавил ui, изменил модельки и т.д.
    
    Co-Authored-By: Mykhailo Govda <72455545+id-zonex@users.noreply.github.com>
    Co-Authored-By: vay4ie <75880193+vay4ie@users.noreply.github.com>

--- Planets/Assets/Scripts/UI/Timer.cs
+++ Planets/Assets/Scripts/UI/Timer.cs
@@ -4,14 +4,11 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Text))]
 public class Timer : MonoBehaviour
 {
-    [SerializeField] private float highScore;
     private Text _text;
-    public Text _highScoreText;
-    public float _time;
+    private float _time;
 
     private void Start()
     {
-        highScore = PlayerPrefs.GetInt("HighScore1");
         _text = GetComponent<Text>();
     }
 
@@ -19,19 +16,6 @@ public class Timer : MonoBehaviour
     {
         _time += Time.deltaTime;
 
-        _text.text = $": {Mathf.Floor(_time)}s";
-
-        ChangeHighRecord();
-    }
-
-    private void ChangeHighRecord()
-    {
-        _highScoreText.text = highScore.ToString();
-
-        if (_time >= highScore)
-        {
-            int i = Mathf.RoundToInt(_time);
-            PlayerPrefs.SetInt("HighScore1", i);
-        }
+        _text.text = $": {Mathf.Floor(_time).ToString()}s" ;
     }
 }
